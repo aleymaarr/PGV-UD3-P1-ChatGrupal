15cb7ffe420dc089dec0c67d4bd83d5ac7ed33a9
aleymarr <aleyba2002@gmail.com>
2024-01-28 19:49:35 +0000

    Feature:Anadiendole funcionaildades al  codigo

--- src/Server/Message.java
+++ src/Server/Message.java
@@ -1,5 +1,33 @@
 package Server;
 
+import common.Utils;
+
 public class Message {
+    private String sender;
+    private String content;
+    private String timestamp;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = Utils.getFormattedCurrentTime();
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String serialize() {
+
+        return String.format("[%s] %s: %s", timestamp, sender, content);
+    }
 
 }
